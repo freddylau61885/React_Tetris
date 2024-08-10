5bdbd0942932d58cc385d2c9c14b0eb62ce99db1
freddylau89 <freddy.lau@nikoyo.com>
2024-08-10 16:05:39 -0500

    Created cells in Stage component

--- react-tetris/src/components/Stage.js
+++ react-tetris/src/components/Stage.js
@@ -4,7 +4,13 @@ import Cell from './Cell';
 
 const Stage = ({ stage }) => (
     <div>
-        <Cell />
+        {
+            stage.map(
+                row => row.map(
+                    (cell, x) => <Cell key={x} type={cell[0]} />
+                )
+            )
+        }
     </div>
 )
 
