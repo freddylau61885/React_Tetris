8d7383f07fc0acf3712c6689f9212ef426f42294
freddylau89 <freddy.lau@nikoyo.com>
2024-08-14 13:34:38 -0500

    Created stage style

--- react-tetris/src/components/Stage.js
+++ react-tetris/src/components/Stage.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { StyledStage } from "./styles/StyledStage";
 import Cell from './Cell';
 
 const Stage = ({ stage }) => (
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
         {
             stage.map(
                 row => row.map(
@@ -11,7 +11,7 @@ const Stage = ({ stage }) => (
                 )
             )
         }
-    </div>
-)
+    </StyledStage>
+);
 
 export default Stage;
