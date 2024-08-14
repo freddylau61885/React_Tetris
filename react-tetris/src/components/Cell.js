06af3932405601b2288691adab25edba09b2f143
freddylau61885 <lauk61885@gmail.com>
2024-08-13 21:03:42 -0500

    Created styled for tetrominos cell

--- react-tetris/src/components/Cell.js
+++ react-tetris/src/components/Cell.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <div>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>
         cell
-    </div>
+    </StyledCell>
 )
 
 export default Cell;
