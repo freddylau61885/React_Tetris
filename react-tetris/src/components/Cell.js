b0b457a70c5d177e407a9af04458c08815e55053
freddylau61885 <lauk61885@gmail.com>
2024-08-14 13:34:38 -0500

    Created stage style

--- react-tetris/src/components/Cell.js
+++ react-tetris/src/components/Cell.js
@@ -3,9 +3,7 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={'L'} color={TETROMINOS['L'].color}>
-        cell
-    </StyledCell>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color} />        
 )
 
 export default Cell;
